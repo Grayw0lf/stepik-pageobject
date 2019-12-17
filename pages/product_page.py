0cd1c830537187c1967e64a1b78645c8a64ff41f
Graywolf <akosheev@rambler.ru>
2019-12-17 14:49:42 +0300

    Change product and base page, add message.

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,3 @@
-import math
-from selenium.common.exceptions import NoAlertPresentException
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
@@ -18,3 +16,15 @@ class ProductPage(BasePage):
         item = self.browser.find_element(*ProductPageLocators.GOOD).text
         items_check = self.browser.find_element(*ProductPageLocators.GOOD_CHECK).text
         assert item == items_check, "Wrong item or name of item!"
+
+    def should_be_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not presented"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -15,3 +15,26 @@ def test_guest_can_add_product_to_basket(browser, link):
     cart.solve_quiz_and_get_code()
     cart.price_correct()
     cart.items_correct()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    cart = ProductPage(browser, link)
+    cart.open()
+    cart.add_to_cart()
+    cart.should_not_be_success_message()
+
+
+def test_user_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    cart = ProductPage(browser, link)
+    cart.open()
+    cart.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
+    cart = ProductPage(browser, link)
+    cart.open()
+    cart.add_to_cart()
+    cart.success_message_is_disappeared()
