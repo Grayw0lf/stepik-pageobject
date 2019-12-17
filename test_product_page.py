ac3f89b00eb6064256978a941c5c4b5a3ea8d9f2
Graywolf <akosheev@rambler.ru>
2019-12-17 22:10:21 +0300

    Change main and base page, change login link test.

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 
 product_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
@@ -15,6 +16,23 @@ def test_guest_can_add_product_to_basket(browser, link):
     cart.solve_quiz_and_get_code()
     cart.price_correct()
     cart.items_correct()
+
+
+@pytest.mark.login_link
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.login_link
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    login = LoginPage(browser, link)
+    login.open()
+    login.go_to_login_page()
+    login.should_be_login_page()
 
 
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
