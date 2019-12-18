7a93287fc268e1a69e20bb7bc10c0fb03656af89
Graywolf <akosheev@rambler.ru>
2019-12-18 23:34:57 +0300

    Final commit.

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import NoSuchElementException, \
     NoAlertPresentException, \
     TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from .locators import BasePageLocators, MainPageLocators
 
 
@@ -42,7 +42,7 @@ class BasePage:
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).\
-                until(EC.presence_of_element_located((how, what)))
+                until(ec.presence_of_element_located((how, what)))
         except TimeoutException:
             return True
         return False
@@ -50,7 +50,7 @@ class BasePage:
     def is_disappeared(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
+                until_not(ec.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
 
